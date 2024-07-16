27f70032875e9bdb0ef7710447d32dcaf85014d2
Oky <146697026+rhizaqi@users.noreply.github.com>
2024-07-16 18:42:27 +0700

    feat: bisa search

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -8,6 +8,11 @@ const Home = () => {
   const router = useRouter();
   const [dataUsers, setDataUsers] = useState();
   const [userLogin, setUserLogin] = useState({});
+  const [option, setOption] = useState({
+    sortBy: "",
+    order: "",
+    search: "",
+  });
 
   const dataLogin = async (idLogin) => {
     try {
@@ -21,9 +26,19 @@ const Home = () => {
 
   const fetchData = async () => {
     try {
-      const { data } = await axios.get("/users");
-      // console.log(data.users, `????`);
-      setDataUsers(data.users);
+      if (option.search) {
+        const { data } = await axios.get(`/users/search?q=${option.search}`);
+        setDataUsers(data.users);
+      } else if (option.order && option.sortBy) {
+        const { data } = await axios.get(
+          `/users?sortBy=${option.sortBy}&order=${option.order}`
+        );
+        setDataUsers(data.users);
+      } else {
+        const { data } = await axios.get(`/users`);
+        // console.log(data.users, `????`);
+        setDataUsers(data.users);
+      }
     } catch (error) {
       console.log(error);
       Swal.fire({
@@ -34,6 +49,13 @@ const Home = () => {
       });
     }
   };
+
+  function handleSearch(value) {
+    setOption({
+      ...option,
+      search: value,
+    });
+  }
 
   const handleLogout = () => {
     localStorage.clear();
@@ -49,6 +71,7 @@ const Home = () => {
     dataLogin(idLogin);
   }, []);
 
+  console.log(option, `<< option bisa kah?`);
   return (
     <div className="flex flex-col gap-10 p-4">
       <div className="flex justify-between">
@@ -73,21 +96,88 @@ const Home = () => {
               {userLogin.company?.department}
             </div>
           </div>
-          <div className="flex gap-2 items-center">
-            <input
-              className="rounded-xl p-2 text-light"
-              placeholder="search"
-              type="text"
-              // onChange={(event) => handleSearch(event.target.value)}
-            />
-            <h2> Search </h2>
+          <div className="flex items-center">
+            <div className="flex justify-around gap-5">
+              <input
+                className="rounded-xl p-2 text-black"
+                placeholder="search"
+                type="text"
+                value={option.search}
+                onChange={(event) => handleSearch(event.target.value)}
+              />
+              <button onClick={fetchData}>Search</button>
+            </div>
           </div>
           <div>
-            <button> Name </button>
-            {/* drop down firstName, lastName */}
+            <button
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="dropdown"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+            >
+              Sort {" "}
+              <svg
+                className="w-2.5 h-2.5 ms-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            <div
+              id="dropdown"
+              className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                <li>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Earnings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div>
-            <button> Date </button>
+            <button> Order by </button>
             {/* ASC - DESC */}
           </div>
         </div>
